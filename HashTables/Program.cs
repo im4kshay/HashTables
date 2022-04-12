d90412fc8f650a2df6bc61316f53557f7b0d2bf3
im4kshay <akshaysayre7@gmail.com>
2022-04-12 19:59:15 +0530

    Ability to find frequency of words in a sentence like “To be or not to be”

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -13,6 +13,25 @@ namespace HashTables
             //Welcome Message
             Console.WriteLine("=-=-=-=-=-Welcome To The Hash Tables Program-=-=-=-=-=");
 
+            //Object of class MyMapNode
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+
+            //Add data
+            myMapNode.Add("0", "To");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "or");
+            myMapNode.Add("3", "not");
+            myMapNode.Add("4", "to");
+            myMapNode.Add("5", "be");
+
+            //Get Value and Display
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th index  value: " + hash5);
+
+            //Get Value and Display
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd index  value: " + hash2);
+
             Console.ReadLine();
         }
     }
